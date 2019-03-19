eafff759b1bc2f6bcdbf62f85c87af17c219d0b9
Jakub Lemka <lemka.jakub@gmail.com>
2019-03-19 17:53:29 +0100

    Lab2 - corrections after code review

--- Lab2/main.js
+++ Lab2/main.js
@@ -1,53 +1,55 @@
+var uuid = require('uuid/v4');
+
 var games = [
   {
-    id: 1,
+    id: uuid(),
     homeTeam: 'Arka Gdynia',
     awayTeam: 'Manchester United',
     scoreHomeTeam: {
       numOfGoals: 1,
-      goalScorers: [{ player: 'Siemaszko', min: 89 }],
+      goalScorers: [{ player: 'Siemaszko', min: 89 }]
     },
     scoreAwayTeam: {
       numOfGoals: 2,
       goalScorers: [
         { player: 'Rashford', min: 60 },
-        { player: 'Lukaku', min: 4 },
-      ],
+        { player: 'Lukaku', min: 4 }
+      ]
     },
-    isFriendlyGame: false,
+    isFriendlyGame: false
   },
   {
-    id: 2,
+    id: uuid(),
     homeTeam: 'Lech Poznań',
     awayTeam: 'Legia Warszawa',
     scoreHomeTeam: {
       numOfGoals: 0,
-      goalScorers: [],
+      goalScorers: []
     },
     scoreAwayTeam: {
       numOfGoals: 2,
-      goalScorers: [],
+      goalScorers: []
     },
-    isFriendlyGame: false,
+    isFriendlyGame: false
   },
   {
-    id: 3,
+    id: 999,
     homeTeam: 'Real Madryt',
     awayTeam: 'Atletico Madryt',
     scoreHomeTeam: {
       numOfGoals: 0,
-      goalScorers: [],
+      goalScorers: []
     },
     scoreAwayTeam: {
       numOfGoals: 3,
       goalScorers: [
         { player: 'Koke', min: 1 },
         { player: 'Koke', min: 4 },
-        { player: 'Morata', min: 54 },
-      ],
+        { player: 'Morata', min: 54 }
+      ]
     },
-    isFriendlyGame: true,
-  },
+    isFriendlyGame: true
+  }
 ];
 
 function getScoreByIndex(index) {
@@ -83,43 +85,77 @@ function getAllFriendlyGames() {
   });
 }
 
-function getGameByTeam(team) {
-    return games.filter(function(game) {
-      return game.homeTeam === team || game.awayTeam === team;
+function getGamesByTeam(team) {
+  return games.filter(function(game) {
+    return game.homeTeam === team || game.awayTeam === team;
+  });
+}
+
+function getAllScores(allGames = games) {
+  function forEachGameWorker(game, index) {
+    console.log(getScoreByIndex(index));
+  }
+  return allGames.forEach(forEachGameWorker);
+}
+
+function scoreGoal(index, isHomeTeam, minute, player) {
+  if (isHomeTeam) {
+    games[index].scoreHomeTeam.numOfGoals++;
+    games[index].scoreHomeTeam.goalScorers.push({
+      player: player,
+      min: minute
     });
+  } else {
+    games[index].scoreAwayTeam.numOfGoals++;
+    games[index].scoreAwayTeam.goalScorers.push({
+      player: player,
+      min: minute
+    });
+  }
 }
-
-function forEachGameWorker(el, index) {
-  console.log(getScoreByIndex(index));
-}
-
-function getAllScores() {
-  return games.forEach(forEachGameWorker);
-}
-
 
 function addGame(team1, score1, team2, score2, isFriendly) {
   var newGame = {
-    id: games.length,
+    id: uuid(),
     homeTeam: team1,
     awayTeam: team2,
     scoreHomeTeam: {
       numOfGoals: score1,
-      goalScorers: [],
+      goalScorers: []
     },
     scoreAwayTeam: {
       numOfGoals: score2,
-      goalScorers: [],
+      goalScorers: []
     },
-    isFriendlyGame: isFriendly,
+    isFriendlyGame: isFriendly
   };
   games.push(newGame);
 }
-
-addGame('FC Barcelona', 2, 'PSG', 1, false);
 
 function removeGameByIndex(index) {
   games.splice(index, 1);
 }
 
+// TESTS
+
+console.log('It should be 3: ', games.length);
+addGame('FC Barcelona', 2, 'PSG', 1, false);
+console.log('It should be 4: ', games.length);
 removeGameByIndex(3);
+console.log('It should be 3 again: ', games.length);
+
+console.log(games[1]);
+scoreGoal(1, true, 25, 'Gytkjaer');
+console.log(games[1]);
+
+console.log(getAllFriendlyGames());
+
+console.log(getScoreById(999));
+
+addGame('Arka Gdynia', 5, 'FC Barcelona', 1, false);
+scoreGoal(3, true, 88, 'Janota');
+console.log(getScoreByIndex(3));
+
+console.log(getGamesByTeam('Arka Gdynia'));
+
+console.log(getAllScores(getGamesByTeam('Arka Gdynia')));
