9d7d06393eea109a6fd6a0ef1d865af34df3cc89
Jakub Lemka <lemka.jakub@gmail.com>
2019-04-14 09:35:42 +0200

    lab5 - first react app

---
+++ Lab5/reactapp/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function FootballGame() {
+  return <h2>Arka Gdynia 2:2 Lech Poznań</h2>;
+}
+
+ReactDOM.render(<FootballGame />, document.getElementById('root'));
