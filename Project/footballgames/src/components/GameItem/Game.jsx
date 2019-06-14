eb05823be00af3b659a6294f3a8679bbd15796f8
Jakub Lemka <jakub.lemka@jitsolutions.pl>
2019-06-14 10:22:59 +0200

    Project | Games list item styled

--- Project/footballgames/src/components/GameItem/Game.jsx
+++ Project/footballgames/src/components/GameItem/Game.jsx
@@ -1,39 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import GameItemContainer from './styled/GameItemContainer';
+import GameLink from './styled/GameLink';
 
 const getScore = game =>
   `${game._homeTeam} ${game._scoreHome} : ${game._scoreAway} ${game._awayTeam}`;
 
-const Container = styled.div`
-  background: #ffffff;
-  margin: 0 auto 15px;
-  padding: 10px 0;
-  text-align: center;
-  color: #000000;
-  width: 80%;
-
-  &:hover {
-    opacity: 0.7;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
-`;
-
 const Game = ({ game }) => (
-  <StyledLink to={`/games/${game._id}`}>
-    <Container>{getScore(game)}</Container>
-  </StyledLink>
+  <GameLink to={`/games/${game._id}`}>
+    <GameItemContainer>{getScore(game)}</GameItemContainer>
+  </GameLink>
 );
 
 export default Game;
---
+++ Project/footballgames/src/components/GameItem/styled/GameItemContainer.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const GameItemContainer = styled.div`
+  box-sizing: border-box;
+  background: #ffffff;
+  margin: 0 auto 15px;
+  padding: 10px 0;
+  text-align: center;
+  color: #000000;
+  width: 80%;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export default GameItemContainer;
